6b377a0e58315e556c8a5e32ca894a87d34b425e
Leha1007 <alukasenko08@gmail.com>
2024-07-29 16:07:41 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,15 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+a = len(numbers)
+primes = []
+not_primes = []
+for j in range(a):
+    primes.append(numbers[j])
+    for k in range(2, numbers[j]):
+        if numbers[j] % k == 0:
+            not_primes.append(numbers[j])
+            primes.remove(numbers[j])
+            break
+
+print('Primes:', primes)
+print('Not-primes', not_primes)
+
